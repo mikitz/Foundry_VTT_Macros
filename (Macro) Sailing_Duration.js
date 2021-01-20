52c05796736ba23ebfe34ad1cc2484572afc4d60
Michael Fitzgerald <mikitz@users.noreply.github.com>
2021-01-19 18:43:44 -0600

    merged sailing duration and cargo transport macros

--- (Macro) Sailing_Duration.js
+++ (Macro) Sailing_Duration.js
@@ -25,6 +25,12 @@ let vMilesPerDay = ''
 let vDaysToDestination = ''
 let vRemaining = ''
 let vMessage = '<H2>SAILING DURATION</H2> <P>'
+let vPricePerMile = 0.08
+let vPricePerPound = 0.02
+let vHandlingFee = 0
+let vShippingFee = 0
+let vRevenue = 0
+let vWeight = 0
 // INITIAL DIALOG
 // Dropdown for hours spent sailing each day
 let ddShifts = `<form action="/action_page.php">
@@ -83,13 +89,21 @@ let tiDistance = `
                 <label for="distance">Distance to Destination (MPH): </label>
                 <input id="distance" name="num" type="number" min="0"></input>
             </div>
-        </form>`;      
+        </form>`;
+// Text input for the weight of the cargo
+let tiWeight = `
+            <div class="form-group">
+                <label for="weight">Cargo Weight (lbs): </label>
+                <input id="weight" name="num" type="number" min="0"></input>
+            </div>
+        </form>`;         
 // Define the initial dialog
 let dInitial = new Dialog ({
     title: "Sailing Duration",
     content: `${ddShifts} <P>
                 ${ddSpeed}
-                ${tiDistance}`,
+                ${tiDistance}
+                ${tiWeight}`,
     buttons: {
         ok: {
             id: "1",
@@ -102,6 +116,8 @@ let dInitial = new Dialog ({
                 vSpeed = html.find('#speed').val()
                 // Get the distance to destination from the user input
                 vDistance = Number(html.find("#distance")[0].value)
+                // Get the weight of the cargo from the user input
+                vWeight = Number(html.find("#weight")[0].value)
                 // Calcualte the number of hours sailed per day
                 vHours = vShifts * 8
                 // Calculate the distance travelled in a day
@@ -110,6 +126,12 @@ let dInitial = new Dialog ({
                 vDaysToDestination = Math.floor(vDistance / vMilesPerDay)
                 // Calculate the number of remaining hours from above
                 vRemaining = Math.ceil(24 * ((vDistance / vMilesPerDay) - vDaysToDestination))
+                // Calculate the Shipping Fee
+                vShippingFee = +((vPricePerMile * vDistance) + (vPricePerPound * vWeight)).toFixed(2)
+                // Calculate the Handling Fee
+                vHandlingFee = +((vShippingFee * 0.05) * vShifts).toFixed(2)
+                // Calculate total revenue
+                vRevenue = vShippingFee + vHandlingFee
                 // Log to the console for debugging purposes
                 console.log(`VARIABLES
                             Shifts: ${vShifts}
@@ -122,7 +144,10 @@ let dInitial = new Dialog ({
                 // Build the message
                 vMessage += `While sailing for <I>${vHours} hours</I> per day at a speed of <I>${vSpeed} MPH</I>, 
                             you will cover a distance of <I>${vDistance} miles</I> in about <B>${vDaysToDestination} days and ${vRemaining} hours</B>. 
-                            Please note that this assumes nothing will go wrong.`
+                            Please note that this assumes nothing will go wrong.
+                            <H2>CARGO TRANSPORTATION</H2>
+                            Transporting cargo with a weight of <I>${vWeight} lbs</I> over a distance of <I>${vDistance} miles</I> will net you a revenue of <B>${vRevenue} gp</B>, 
+                            comprised of a handling fee of <I>${vHandlingFee} gp</I> and a shipping fee of <I>${vShippingFee} gp</I>.`
                 // Print the message
                 fPrintMessage(vMessage)
             }
--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 All the macros that I use in FoundryVTT
 
 **TO-DO LIST**
+* [x] Beer Brewing
+   * [x] Outputs quality of barrel
+   * [x] Need to account for natual 1s
+* [x] Recreational Drugs
+    * [x] Finish up the bestiary type drugs
+* [x] Sailing Travel Time Calculator
 * Strongholds & Followers
 * Magical Experiments (Alternative to Getting Spells Every Level)
     * Complete descriptions for all spells and their respective outcomes
@@ -15,15 +21,7 @@ All the macros that I use in FoundryVTT
         * Mining
     * Crafting
         * ...
-* Cargo Transportation Fee Calculator
-    * ...
-* [x] Sailing Travel Time Calculator
 * Tavern Games
-* [x] Beer Brewing
-   * [x] Outputs quality of barrel
-   * [x] Need to account for natual 1s
-* [x] Recreational Drugs
-    * [x] Finish up the bestiary type drugs
 
 **LONG-TERM TO-DO LIST**
 * Convert Foundry Roll Tables to JavaScript Object Arrays (For Website)
